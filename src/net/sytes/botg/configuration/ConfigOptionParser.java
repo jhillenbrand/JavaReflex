a7689e28aa8187d9b5b998c4a7c41699d011ee9d
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2022-06-05 10:55:18 +0200

    changed configoption package

--- src/net/sytes/botg/configuration/ConfigOptionParser.java
+++ src/net/sytes/botg/configuration/ConfigOptionParser.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.sytes.botg.datatypes.ConfigOption;
 import net.sytes.botg.reflection.JFlex;
 import net.sytes.botg.reflection.JFlex.JFlexException;
 
